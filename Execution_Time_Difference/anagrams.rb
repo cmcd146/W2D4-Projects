760725e90785d376af284340ebb8336228018ec9
Chris_McDonald <mcdonaldchristopherkevin@gmail.com>
2018-08-30 17:04:12 -0700

    Finish second_anagram? and third_anagram?

--- Execution_Time_Difference/anagrams.rb
+++ Execution_Time_Difference/anagrams.rb
@@ -6,9 +6,28 @@ def first_anagram?(str1,str2)
   words.include?(str2)
 end
 
-def second_anagram(str1, str2)
+def second_anagram?(str1, str2)
+  chars = str1.chars 
+  chars2 = str2.chars
+  
+  chars.each do |ch| 
+    index = chars2.find_index(ch)
+    return false if index == nil 
+    chars2.delete_at(index) 
+  end 
+  chars2.empty?
+end 
+
+def third_anagram?(str1, str2)
+  chars = str1.chars 
+  chars2 = str2.chars 
+  
+  chars.sort! 
+  chars2.sort! 
+  
+  chars == chars2
 end 
 
 if __FILE__ == $PROGRAM_NAME
-  p first_anagram?("elvis", "lives")
+  p third_anagram?("elvis", "lives")
 end
